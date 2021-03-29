f694a7c9177700cb6ba7299184269aead7a3635c
MacaronFR <denisft77@gmail.com>
2021-03-29 11:14:29 +0200

    submodules

--- clientc/include/xml.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLIENTC_XML_H
-#define CLIENTC_XML_H
-
-#include <libxml/tree.h>
-
-xmlNodePtr newProduct(char *mark, char *type, char *model, char ***specification);
-
-#endif //CLIENTC_XML_H
--- site/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Hyperion\WebSite\Router;
-use Hyperion\WebSite\TestController;
-
-require_once "Router.php";
-require_once "Controllers/TestController.php";
-$rt = new Router(new TestController());
-$rt->get("/lol", new TestController());
-$rt->default();
